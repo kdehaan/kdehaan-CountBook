7dddf3044b80e1e998d304e937b5499ae4e5e043
Kevin de Haan <kbehaan@gmail.com>
2017-09-28 13:28:56 -0600

    Listview implementd

--- app/src/main/java/dehaan/kdehaan_countbook/MainActivity.java
+++ app/src/main/java/dehaan/kdehaan_countbook/MainActivity.java
@@ -1,13 +1,91 @@
 package dehaan.kdehaan_countbook;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "countBook.sav";
+
+    private ListView CounterList;
+
+    private ArrayList<Counter> counters = new ArrayList<Counter>();
+    private ArrayAdapter<Counter> adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.createCounter);
+        CounterList = (ListView) findViewById(R.id.CounterList);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                counters.add(new Counter("new counter", 0));
+                adapter.notifyDataSetChanged();
+                saveInFile();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadFromFile();
+        adapter = new ArrayAdapter<Counter>(this, R.layout.list_item, counters);
+        CounterList.setAdapter(adapter);
+    }
+
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<Counter>>() {}.getType();
+            counters = gson.fromJson(in, listType);
+        } catch(FileNotFoundException e) {
+            counters = new ArrayList<Counter>();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(counters, writer);
+            writer.flush();
+            fos.close();
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
