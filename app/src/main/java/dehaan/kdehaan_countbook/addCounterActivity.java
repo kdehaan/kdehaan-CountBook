a974d729413dd82f9e6cb44dab8082a8b7249b62
Kevin de Haan <kbehaan@gmail.com>
2017-09-30 22:33:20 -0600

    basic toast working

--- app/src/main/java/dehaan/kdehaan_countbook/addCounterActivity.java
+++ app/src/main/java/dehaan/kdehaan_countbook/addCounterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -12,6 +13,7 @@ import com.google.gson.Gson;
 public class addCounterActivity extends AppCompatActivity {
 
     private Intent intent;
+    private Toast toast;
 //    private String gsonCounter;
 //    private Counter counter;
 
@@ -22,24 +24,23 @@ public class addCounterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_counter);
 
         intent = getIntent();
-//        gsonCounter = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-//
-//        counter = gson.fromJson(gsonCounter, Counter.class);
-//
-//        TextView textView = (TextView) findViewById(R.id.textView);
-//        textView.setText(counter.toString());
-//        intent.putExtra("gsonCounter", gsonCounter);
-//        setResult(RESULT_OK, intent);
+        toast = new Toast(this);
 
-//        finish();
-
-
-//        finish();
     }
 
+    private boolean isEmpty(EditText editText) {
+        return editText.getText().toString().trim().length() == 0;
+    }
 
     public void confirmNewCounter(View view) {
+
+
+
         EditText nameText = (EditText) findViewById(R.id.editName);
+        if (isEmpty(nameText)) {
+            toast.makeText(this, "Invalid Name", Toast.LENGTH_SHORT).show();
+            return;
+        }
         String nameString = nameText.getText().toString();
 
         EditText initValText = (EditText) findViewById(R.id.editInitVal);
@@ -47,6 +48,11 @@ public class addCounterActivity extends AppCompatActivity {
 
         EditText commentText = (EditText) findViewById(R.id.editComment);
         String commentString = commentText.getText().toString();
+
+
+
+
+//        Counter counter = new Counter("sdf", 1, "");
 
         Counter counter = new Counter(nameString, initValInt, commentString); // int not behaving
 
